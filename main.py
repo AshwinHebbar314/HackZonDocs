cd61a76c35b3add1b1995e5aebbb5dd963d28c42
harshmehta14 <harshymehta@gmail.com>
2023-01-05 14:53:58 +0530

    homepage

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from flask import render_template,Flask
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run() 
