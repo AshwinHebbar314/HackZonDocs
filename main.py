82030353cabe23241afdb1bfc7d4fc6421181284
harshmehta14 <harshymehta@gmail.com>
2023-01-05 15:25:48 +0530

    login and dashboard page

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
 from flask import render_template,Flask
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html',all_track=[[1,"Fitness Tracker","Number","05-01-2023","100"]]) 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+    
 if __name__ == '__main__':
   app.debug = True
   app.run() 
